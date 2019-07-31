71e2a7fd2de8044d623492a822ae3090e3f10843
GchengC <chengccguille@gmail.com>
2019-07-31 11:15:00 -0400

    Se elimina proyecto web de FinalFantasy para poder actualizarlo a Angular 8 en un futuro

--- Practicas/Front/angular-simple-crud/src/app/app.component.ts
+++ Practicas/Front/angular-simple-crud/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Employer } from './models/employer';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+  employerArray: Employer[] = [
+    { id: 1, name: "Ryan", country: "USA" },
+    { id: 2, name: "Angelica", country: "USA" },
+    { id: 3, name: "Joe", country: "USA" }
+  ];
+
+  selectedEmployer: Employer = new Employer();
+
+  addOrEdit() {
+    if (this.selectedEmployer.id === 0) {
+      this.selectedEmployer.id = this.employerArray.length + 1;
+      this.employerArray.push(this.selectedEmployer);
+    }
+
+    this.selectedEmployer = new Employer();
+  };
+
+  openEdit(employer: Employer) {
+    this.selectedEmployer = employer;
+  };
+
+  delete() {
+    if (confirm('Are you sure you want to delete ir?')) {
+      this.employerArray = this.employerArray.filter(x => x != this.selectedEmployer);
+      this.selectedEmployer = new Employer();
+    } 
+  };
+
 }
